773fe77304312437e4f58f7df028582c8b3f7245
Whocares <yuldashevmrb@gmail.com>
2022-09-06 19:37:39 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = second ^ first;
+        first = first ^ second;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if ( divider != 0) {
+            int result = dividend / divider;
+            if (result * divider == dividend)
+                System.out.println("can be divided completely");
+            else
+                System.out.println("cannot be divided completely");
+        } else {
+            System.out.println("division by zero");
+        }
     }
 }
